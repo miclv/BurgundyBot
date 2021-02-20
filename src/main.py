b5e4869c854a9eba9074f187823db40d11ac72e7
Michael Vierling <fp6352ke@minnstate.edu>
2021-02-19 19:54:14 -0600

    Fixed token import

--- src/main.py
+++ src/main.py
@@ -6,13 +6,12 @@ from dotenv import load_dotenv
 load_dotenv()
 Token = os.getenv('DISCORD_TOKEN')
 
-
 class MyClient(discord.Client):
 
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
 
-    async def on_message(selfself, message):
+    async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
 
 
