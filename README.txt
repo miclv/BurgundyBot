7a7ce58229b1ddc5ce28096029fe43d42cea07a2
Michael Vierling <fp6352ke@minnstate.edu>
2021-02-24 12:08:09 -0600

    Added ping command, removed on_message Updated README.txt

--- README.txt
+++ README.txt
@@ -1,3 +1,5 @@
 Author: Michael Vierling
 
 This is a python bot created for my Senior Seminar class at Minnesota State University - Moorhead
+Updates will be made to this GitHub repository
+This code relates to my project because this is my project code
--- src/main.py
+++ src/main.py
@@ -8,19 +8,12 @@ load_dotenv()
 Token = os.getenv('DISCORD_TOKEN')
 
 
-bot = commands.Bot(command_prefix='?')
+bot = commands.Bot(command_prefix='!')
 
-
-#class MyClient(discord.Client):
 
 @bot.event
 async def on_ready():
     print('Logged on as {0}!'.format(bot.user))
-
-
-@bot.event
-async def on_message(message):
-    print('Message from {0.author}: {0.content}'.format(message))
 
 
 @bot.command()
@@ -28,5 +21,4 @@ async def ping(ctx):
     await ctx.send(f'Pong! {round(bot.latency * 1000)}ms')
 
 
-#client = MyClient()
 bot.run(Token)
