a17e81a078387d7ac2853ff5045d84cf72f8f320
Michael Vierling <fp6352ke@minnstate.edu>
2021-02-24 10:42:40 -0600

    Test commit

--- src/main.py
+++ src/main.py
@@ -1,19 +1,32 @@
 import discord
 import os
 
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 Token = os.getenv('DISCORD_TOKEN')
 
-class MyClient(discord.Client):
 
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
-
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+bot = commands.Bot(command_prefix='?')
 
 
-client = MyClient()
-client.run(Token)
+#class MyClient(discord.Client):
+
+@bot.event
+async def on_ready():
+    print('Logged on as {0}!'.format(bot.user))
+
+
+@bot.event
+async def on_message(message):
+    print('Message from {0.author}: {0.content}'.format(message))
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(bot.latency * 1000)}ms')
+
+
+#client = MyClient()
+bot.run(Token)
